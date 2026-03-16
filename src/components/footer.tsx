bdd124542135baba33a0cfcd02486b80bc82f0fa
Elia Franzella <franzella.elia@gmail.com>
2026-03-16 04:20:29 +0100

    feat: Add souce link to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,18 @@
+import Link from "./link";
+
 export default function Footer() {
   return (
     <footer className="border-muted-background-dark text-muted-foreground-dark border-t py-6 text-center text-xs md:text-sm">
       <p>Built with NextJS and Tailwind, hosted on Cloudflare.</p>
 
       <p>© 2026 Elia Franzella. All rights reserved.</p>
+
+      <Link
+        href="https://www.github.com/nin93/portfolio"
+        className="underline underline-offset-2"
+      >
+        View source code
+      </Link>
     </footer>
   );
 }
